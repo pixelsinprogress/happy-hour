843b376889745a9b87350bbe5ed83351eb9335cf
Nitin Sampathi <nitinsampathi@Nitins-MacBook-Pro.local>
2018-03-18 21:08:00 -0400

    first commit

--- react-ui/src/App.js
+++ react-ui/src/App.js
@@ -1,17 +1,46 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import * as firebase from 'firebase';
+import ReactMapboxGl, { Layer, Feature } from "react-mapbox-gl";
+
+var config = {
+    apiKey: "AIzaSyAnLoOrNYLClAbK7xrA7JnFxZtycHA7_Fk",
+    authDomain: "fir-test-1ffc9.firebaseapp.com",
+    databaseURL: "https://fir-test-1ffc9.firebaseio.com",
+    projectId: "fir-test-1ffc9",
+    storageBucket: "fir-test-1ffc9.appspot.com",
+    messagingSenderId: "213145455350"
+};
+
+firebase.initializeApp(config);
+
+
+
+const Map = ReactMapboxGl({
+  accessToken: "pk.eyJ1Ijoibml0aW45MyIsImEiOiJjaXpkam5jZTIyYmM2MndvOHZrN2d0bXBvIn0.zZL24G9KtqbOv7d9m775xQ"
+});
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
       message: null,
-      fetching: true
+      fetching: true,
+      location: "Baltimore"
     };
   }
 
   componentDidMount() {
+    const rootRef = firebase.database().ref().child('react');
+    const locationRef = rootRef.child('location');
+    locationRef.on('value', snap => {
+      this.setState({
+        location: snap.val()
+      })
+    });
+
+    /*
     fetch('/api')
       .then(response => {
         if (!response.ok) {
@@ -30,6 +59,7 @@ class App extends Component {
           fetching: false
         });
       })
+    */
   }
 
   render() {
@@ -37,19 +67,16 @@ class App extends Component {
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <h2>Welcome to {this.state.location}</h2>
         </div>
-        <p className="App-intro">
-          {'This is '}
-          <a href="https://github.com/mars/heroku-cra-node">
-            {'create-react-app with a custom Node/Express server'}
-          </a><br/>
-        </p>
-        <p className="App-intro">
-          {this.state.fetching
-            ? 'Fetching message from API'
-            : this.state.message}
-        </p>
+        <Map
+          style="mapbox://styles/mapbox/streets-v9"
+          center= {[-76.6122, 39.2904]}
+          containerStyle={{
+            height: "100vh",
+            width: "100vw"
+          }}>
+        </Map>
       </div>
     );
   }
